b6a8891982ed93bbb9bd3168caa8839988bbb7e1
bunga nadhira <bunganadhira123@gmail.com>
2023-11-11 14:25:02 +0700

    submission1

--- routes/api.php
+++ routes/api.php
@@ -11,7 +11,7 @@ use App\Http\Controllers\PostController;
 | Here is where you can register API routes for your application. These
 | routes are loaded by the RouteServiceProvider within a group which
 | is assigned the "api" middleware group. Enjoy building your API!
-|
+| bunga nadhira
 */
 
 Route::get('posts', [PostController::class, 'index']);
